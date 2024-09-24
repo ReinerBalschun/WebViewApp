b175cba180301b09a181679269e16b6746ea9cb1
Reiner Balschun <reinermbalschun>
2024-09-24 20:34:14 +0200

    First Commit

---
+++ windows/WebViewApp/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::WebViewApp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::WebViewApp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
